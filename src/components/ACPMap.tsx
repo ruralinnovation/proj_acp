467012ce20fb7f5cb095e3216be6ced3560fd814
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2024-06-26 13:32:40 -0400

    working color scale example

--- src/components/ACPMap.tsx
+++ src/components/ACPMap.tsx
@@ -1,4 +1,7 @@
+import { useCallback, useState } from 'react';
+
 import Map, {Source, Layer} from 'react-map-gl';
+import { MapLayerMouseEvent } from 'mapbox-gl';
 
 import style from './styles/ACPMap.module.css';
 
@@ -7,35 +10,69 @@ import type { FeatureCollection } from "geojson";
 import acp_dta_geojson from '../data/acp_new_england.json';
 const acp_dta = acp_dta_geojson as FeatureCollection;
 
-
 import type {FillLayer} from 'react-map-gl';
 
-// Subscribed_2024.02.01
+import { format } from 'd3-format';
+
+// export const dataLayer: FillLayer = {
+//   id: 'data',
+//   type: 'fill',
+//   paint: {
+//     'fill-color': {
+//       property: 'Percent_2024.02.01',
+//       stops: [
+//         [0, '#3288bd'],
+//         [10, '#66c2a5'],
+//         [20, '#abdda4'],
+//         [30, '#e6f598'],
+//         [40, '#ffffbf'],
+//         [50, '#fee08b'],
+//         [60, '#fdae61'],
+//         [70, '#f46d43'],
+//         [80, '#d53e4f']
+//       ]
+//     },
+//     'fill-opacity': 0.8
+//   }
+// };
+
 export const dataLayer: FillLayer = {
   id: 'data',
   type: 'fill',
   paint: {
-    'fill-color': {
-      property: 'Subscribed_2024.02.01',
-      stops: [
-        [0, '#3288bd'],
-        [1, '#66c2a5'],
-        [2, '#abdda4'],
-        [3, '#e6f598'],
-        [4, '#ffffbf'],
-        [5, '#fee08b'],
-        [6, '#fdae61'],
-        [7, '#f46d43'],
-        [8, '#d53e4f']
-      ]
-    },
+    'fill-color': [
+      'interpolate',
+      ['linear'],
+      // Use 'coalesce' to handle NaN values
+      ['coalesce', ['get', 'Percent_2024.02.01'], -99],
+      // Define the stops for interpolation
+      -99, 'rgba(208, 210, 206, 0.7)', // Color for NaN values
+      0, 'rgba(163, 226, 181, 0.7)',
+      50, 'rgba(86, 131, 116, 0.7)',
+      100, 'rgba(22, 52, 62, 0.7)'
+    ],
     'fill-opacity': 0.8
   }
 };
-
 const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function ACPMap() {
+
+  const [ hoverInfo, setHoverInfo ] = useState<any>(null);
+
+  const onHover: ((e: MapLayerMouseEvent) => void) | undefined = useCallback((event: MapLayerMouseEvent) => {
+    const {
+      features,
+      point: { x, y }
+    } = event;
+    const hoveredFeature = features && features[0];
+
+    console.log("Event is ", hoveredFeature);
+  
+    // prettier-ignore
+    setHoverInfo(hoveredFeature && { feature: hoveredFeature, x, y });
+
+  }, []);
 
   return (
     <>
@@ -49,19 +86,24 @@ function ACPMap() {
             mapStyle="mapbox://styles/mapbox/light-v9"
             mapboxAccessToken={MAPBOX_TOKEN}
             interactiveLayerIds={['data']}
+            onMouseMove={onHover}
         >
             <Source type="geojson" data={acp_dta}>
                 <Layer {...dataLayer} />
             </Source>
-            {/* {hoverInfo && (
-                <div className="tooltip" style={{left: hoverInfo.x, top: hoverInfo.y}}>
-                    <div>State: {hoverInfo.feature.properties.name}</div>
-                    <div>Median Household Income: {hoverInfo.feature.properties.value}</div>
-                    <div>Percentile: {(hoverInfo.feature.properties.percentile / 8) * 100}</div>
-                </div>
-            )} */}
         </Map>
-
+        {hoverInfo && (
+            <div className={style['tooltip']} style={{left: hoverInfo.x, top: hoverInfo.y}}>
+                <h2>Zipcode: {hoverInfo.feature.properties.Zipcode}</h2>
+                <p>
+                  Eligble: {hoverInfo.feature.properties['Eligible']}<br/>
+                  Subscribed: {hoverInfo.feature.properties['Subscribed_2024.02.01']}<br/>
+                  Percent subscribed: {format(".3")(hoverInfo.feature.properties['Percent_2024.02.01']) + "%"}<br/>
+                  Change subscribed: {format(",")(hoverInfo.feature.properties['Change_Subscribed_2024.02.01'])}<br/>
+                  Change percent: {format(",.2s")(hoverInfo.feature.properties['Change_Percent_2024.02.01']) + "%"}<br/>
+                </p>
+            </div>
+        )}
       </div>
     </>
   )
