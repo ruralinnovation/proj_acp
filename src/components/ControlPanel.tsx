f809d9d5504d0cd4605a14c0465018a5ba163bee
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2024-07-04 18:01:53 -0400

    Added logos

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -3,10 +3,15 @@ import style from './styles/ControlPanel.module.css';
 import CategoricalLegend from './CategoricalLegend';
 import { ACPMapScale } from '../constants';
 
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useCallback } from 'react';
+
+import debounce from 'lodash/debounce';
 
 import Slider from '@mui/material/Slider';
 import { useState } from 'react';
+
+import lisc_logo from "../assets/Rural_stacked corrected png.png";
+import cori_logo from "../assets/Full-Logo_CORI_Black.svg";
 
 const valid_dates: { value: number} [] = [];
 const date_lookup: Record<number, string> = {};
@@ -41,7 +46,7 @@ function ControlPanel({ setParentDate, setParentLayerFilter }: ControlPanelProps
   const [ proportionFilter, setProportionFilter ] = useState<number[]>([0, 100]);
 
   const [month, year]: string[] = date.split("/");
-  const variable_suffix: string = year + '.' + month.padStart(2, '0') + '.01'
+  const variable_suffix: string = year + '.' + month.padStart(2, '0') + '.01';
 
   const handleDateChange = (_event: Event, newValue: number | number[]) => {
 
@@ -51,24 +56,34 @@ function ControlPanel({ setParentDate, setParentLayerFilter }: ControlPanelProps
     }
 
   };
-
+  
   const handleProportionChange = (_event: Event, newValue: number | number[]) => {
     const isNumberArray: boolean = Array.isArray(newValue) && newValue.every(item => typeof item === 'number');
     if (isNumberArray) {
       const numberArray = newValue as number[];
       setProportionFilter(numberArray);
+      debouncedSetParentLayerFilter(numberArray);
+    }
+  };
+
+  const debouncedSetParentLayerFilter = useCallback(
+    debounce((numberArray: number[]) => {
       const layer_filter = [
         'all',
         ['>=', ['get', 'Percent_' + variable_suffix], numberArray[0]],
         ['<=', ['get', 'Percent_' + variable_suffix], numberArray[1]]
       ];
       setParentLayerFilter(layer_filter);
-    }
-  };
-  
+    }, 300),
+    [variable_suffix, setParentLayerFilter]
+  );
 
   return (
     <div className={style['control-panel']}>
+      <div className={style["logos-wrapper"]}>
+        <img className={style["logo"]} src={lisc_logo} alt="Rural LISC logo"/>
+        <img className={style["logo"]} src={cori_logo} alt="CORI logo"/>
+      </div>
       <h1>ACP Enrollment</h1>
       <p>
         Map showing ACP enrollment by zipcode by month. Hover over a zipcode to see details.
@@ -95,7 +110,7 @@ function ControlPanel({ setParentDate, setParentLayerFilter }: ControlPanelProps
         <Slider
           getAriaLabel={() => 'Percent subscribed'}
           value={proportionFilter}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="off"
           onChange={handleProportionChange}
         />
       </div>
