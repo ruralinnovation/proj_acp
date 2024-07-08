95256aca54c61a45b3c8b6f78f3802bd1b0b3250
Drew Rosebush <drew.rosebush@gmail.com>
2024-07-08 10:17:08 -0700

    check the max participation value
    https://ruralinnovation.atlassian.net/browse/ACP-15?atlOrigin=eyJpIjoiM2E2ZTk0NzU4ZTdkNGYzNWJmZmVhNzU0NDAwMzE4MjciLCJwIjoiaiJ9

---
+++ R/data/eda_acp_dta.R
@@ -0,0 +1,12 @@
+## prompt: Check participation rate range
+
+library(dplyr)
+con <- cori.db::connect_to_db('proj_acp')
+dta <- cori.db::read_db(con, 'acp_dta_zip')
+
+
+## check when it's above 100
+check <- dta %>% 
+  filter(
+    variable == 'percent'
+  ) 
